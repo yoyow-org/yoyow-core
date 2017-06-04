058a74fbc96cf02133ed0f720034f72a6b61da6d
Abit <abitmore@users.noreply.github.com>
2017-06-05 00:32:44 +0200

    Fix latency check error

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -556,6 +556,7 @@ namespace detail {
                                 std::vector<fc::uint160_t>& contained_transaction_message_ids) override
       { try {
 
+         auto latency = fc::time_point::now() - blk_msg.block.timestamp;
          if (!sync_mode || blk_msg.block.block_num() % 10000 == 0)
          {
             const auto& witness_account = _chain_db->get_account_by_uid( blk_msg.block.witness );
@@ -569,7 +570,6 @@ namespace detail {
                  ("w",witness_account.name)
                  ("i",last_irr)("d",blk_msg.block.block_num()-last_irr) );
          }
-         auto latency = fc::time_point::now() - blk_msg.block.timestamp;
          FC_ASSERT( (latency.count()/1000) > -5000, "Rejecting block with timestamp in the future" );
 
          try {
