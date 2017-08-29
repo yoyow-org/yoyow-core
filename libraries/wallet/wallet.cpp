140422f26f3265d85c1a3200b64a578e8a11d66f
yoyowd <yoyowd@users.noreply.github.com>
2017-08-30 00:18:03 +0200

    yy WIP. CLI info command tweak

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -507,10 +507,11 @@ public:
       fc::mutable_variant_object result;
       result["head_block_num"] = dynamic_props.head_block_number;
       result["head_block_id"] = dynamic_props.head_block_id;
+      result["head_block_time"] = dynamic_props.time;
       result["head_block_age"] = fc::get_approximate_relative_time_string(dynamic_props.time,
                                                                           time_point_sec(time_point::now()),
                                                                           " old");
-      result["next_maintenance_time"] = fc::get_approximate_relative_time_string(dynamic_props.next_maintenance_time);
+      //result["next_maintenance_time"] = fc::get_approximate_relative_time_string(dynamic_props.next_maintenance_time);
       result["chain_id"] = chain_props.chain_id;
       result["participation"] = (100*dynamic_props.recent_slots_filled.popcount()) / 128.0;
       result["active_witnesses"] = global_props.active_witnesses;
