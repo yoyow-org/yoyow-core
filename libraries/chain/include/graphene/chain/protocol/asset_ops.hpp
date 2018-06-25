92ab2d319d23276e879375db943dc18a5a076245
yoyowd <yoyowd@users.noreply.github.com>
2018-06-26 01:16:59 +0200

    Mainnet 0.3: set default fee for new ops

--- libraries/chain/include/graphene/chain/protocol/asset_ops.hpp
+++ libraries/chain/include/graphene/chain/protocol/asset_ops.hpp
@@ -103,9 +103,9 @@ namespace graphene { namespace chain {
       };
 
       struct fee_parameters_type { 
-         uint64_t symbol3          = 500000 * GRAPHENE_BLOCKCHAIN_PRECISION;
-         uint64_t symbol4          = 300000 * GRAPHENE_BLOCKCHAIN_PRECISION;
-         uint64_t long_symbol      = 5000   * GRAPHENE_BLOCKCHAIN_PRECISION;
+         uint64_t symbol3          = uint64_t(10)*10000*10000 * GRAPHENE_BLOCKCHAIN_PRECISION;
+         uint64_t symbol4          = uint64_t(10)*10000*10000 * GRAPHENE_BLOCKCHAIN_PRECISION;
+         uint64_t long_symbol      = uint64_t(10)*10000*10000 * GRAPHENE_BLOCKCHAIN_PRECISION;
          uint32_t price_per_kbyte  = GRAPHENE_BLOCKCHAIN_PRECISION;
          uint64_t min_real_fee     = 0;
          uint16_t min_rf_percent   = GRAPHENE_100_PERCENT;
@@ -204,7 +204,7 @@ namespace graphene { namespace chain {
    struct asset_reserve_operation : public base_operation
    {
       struct fee_parameters_type {
-         uint64_t fee              = 20 * GRAPHENE_BLOCKCHAIN_PRECISION;
+         uint64_t fee              = uint64_t(10)*10000*10000 * GRAPHENE_BLOCKCHAIN_PRECISION;
          uint64_t min_real_fee     = 0;
          uint16_t min_rf_percent   = 0;
          extensions_type   extensions;
