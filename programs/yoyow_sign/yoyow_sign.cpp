2d832fd373e3f5f11df98d840eebbd800c0f23bc
necklace <xiangxn@163.com>
2018-09-17 18:18:29 +0800

    add generate_key and private_to_public into yoyow_sign

--- programs/yoyow_sign/yoyow_sign.cpp
+++ programs/yoyow_sign/yoyow_sign.cpp
@@ -22,3 +22,22 @@ string signature(string tx, string wif, string chain_id)
    }
    return string("");
 }
+
+string generate_key(string brain_key, int sequence_number)
+{
+   std::string sequence_string = std::to_string(sequence_number);
+   fc::sha512 h = fc::sha512::hash(brain_key + " " + sequence_string);
+   private_key derived_key = private_key::regenerate(fc::sha256::hash(h));
+   return key_to_wif(derived_key);
+}
+
+string private_to_public(string wif)
+{
+   fc::optional< private_key > privkey = wif_to_key( wif );
+   if( privkey.valid() ) {
+      public_key_type key(privkey->get_public_key());
+
+      return string(key);
+   }
+   return string("");
+}
--- programs/yoyow_sign/yoyow_sign.h
+++ programs/yoyow_sign/yoyow_sign.h
@@ -4,3 +4,7 @@
 using namespace std;
 
 string signature(string tx, string wif, string chain_id);
+
+string generate_key(string brain_key, int sequence_number = 0);
+
+string private_to_public(string wif);
