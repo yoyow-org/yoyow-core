64c559bc20710d079815386efea644c3c561b495
yoyowd <yoyowd@users.noreply.github.com>
2019-05-05 10:52:36 +0800

    add origin_platform == platform for comment

--- libraries/chain/protocol/content.cpp
+++ libraries/chain/protocol/content.cpp
@@ -155,9 +155,12 @@ void post_operation::validate()const
                  "post_operation`s extension post_type is invalid");
        if (*(ext.post_type) == Post_Type::Post_Type_Comment
            || *(ext.post_type) == Post_Type::Post_Type_forward
-           || *(ext.post_type) == Post_Type::Post_Type_forward_And_Modify)
+           || *(ext.post_type) == Post_Type::Post_Type_forward_And_Modify){
+           
            FC_ASSERT(origin_post_pid.valid() && origin_poster.valid() && origin_platform.valid(),
-           "${type} post operation must include origin_post_pid, origin_poster and origin_platform", ("type", *(ext.post_type)));
+               "${type} post operation must include origin_post_pid, origin_poster and origin_platform", ("type", *(ext.post_type)));
+           FC_ASSERT(*(origin_platform) == platform, "platform should be origin_platform. ");
+       }
 
        if (ext.receiptors.valid())
        {
