5a877663e97b6ea3c801a687f856754aebd16d6f
vianull <vianull@outlook.com>
2019-10-23 14:09:57 +0800

    format

--- libraries/chain/db_update.cpp
+++ libraries/chain/db_update.cpp
@@ -1578,7 +1578,7 @@ void database::process_content_platform_awards()
                if (csaf > 0)
                {
                   total_effective_csaf_amount += csaf;
-		  post_effective_casf.emplace_back(std::make_tuple((active_post_object*)&(*apt_itr), csaf, approval_amount.convert_to<int64_t>()));
+                  post_effective_casf.emplace_back(std::make_tuple((active_post_object*)&(*apt_itr), csaf, approval_amount.convert_to<int64_t>()));
                }
             }
 
